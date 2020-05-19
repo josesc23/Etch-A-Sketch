e9156c8da8a1752ba846912f449a4ff36b4c7491
Jose Santiago <josesantiagoc21@gmail.com>
2020-05-19 02:06:05 -0500

    Second Draft. Added most functionality

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-function genDivs(v){ 
-    var container = document.getElementById("container"); // whatever you want to append the rows to:
-    // container.style.display = 'inline-block';
-    container.style.width = '672px';
-    container.style.border = "5px solid red"
+var container = document.getElementById("container");
+var randCol = document.getElementById('randCols');
+var blackColor = document.getElementById('blackCol');
 
-    let size = (672-v-v)/v;
+function genDivs(v){ 
+    
+    container.style.width = '700px';
+    // container.style.height = '700px';
+    container.style.border = "5px solid black"
+    // container.style.margin = "0 auto"
+
+    let size = (700-v-v)/v;
 
     for(var i = 0; i < v; i++){ 
       var row = document.createElement("div"); 
-      row.style.height = size + "px";
+      row.style.height = size  + "px";
+      row.style.margin = "0 auto";
+
       for(var x = 1; x <= v; x++){ 
         var cell = document.createElement("div"); 
         cell.classList.add("cell");
         cell.style.display = 'inline-block';
-        cell.style.border = "1px solid black";
+        cell.style.border = "0.5px solid black";
         cell.style.borderCollapse = 'true';
         cell.style.height = size + "px";
         cell.style.width = size + "px";
-        cell.style.backgroundColor = 'orange';
-        
-        //   cell.className = "gridsquare"; 
-       // cell.innerText = (i * v) + x;
+
+        cell.addEventListener("mouseover", function(e) {
+          // e.target.style.backgroundColor = 'lightblue';
+          e.target.style.backgroundColor = toggleColor();
+
+        });
+
         row.appendChild(cell); 
       } 
       container.appendChild(row); 
     } 
   }
+function clearGrid() {
+  mainColor = false;
+  container.innerHTML = "";
+  container.style.border = "none";
+  let size = "";
+  do {
+    size = parseInt(prompt("New Grid: \n How many squares per side?"));
+    if(!isNaN(size)) {
+      genDivs(size);
+    }
+  }
+  while(isNaN(size));
+}
 
-// genDivs(6);
+genDivs(16);
+
+let mainColor = false;
+randCol.onclick = function() {
+  mainColor = true;
+};
+blackColor.onclick = function() {
+  mainColor = false;
+}
+
+function toggleColor() {
+  
+  if (mainColor) {
+    var color = Math.floor(Math.random() * 16777216).toString(16);
+    return '#000000'.slice(0, -color.length) + color;
+  }
+  else {
+    return '#000000';
+  }
+
+}
